2e1d530212c155a2db6d9f19495e8af8bed6cf6c
Ryo Nakamura <ryo-nakamura@cookpad.com>
2013-01-11 18:16:42 +0900

    Show report view about brakeman and rbp

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,9 +1,12 @@
 class Report < ActiveRecord::Base
-  has_many :warnings
+  has_many :warnings, :order => "modified_at DESC"
 
   scope :descending, lambda {
     order("created_at DESC")
   }
+
+  delegate :brakeman, :to => :warnings
+  delegate :rails_best_practices, :to => :warnings
 
   def self.create_with_warnings(warning_attributes_set)
     create.tap do |report|
@@ -15,5 +18,9 @@ class Report < ActiveRecord::Base
 
   def self.latest
     descending.first
+  end
+
+  def brakeman
+    warnings.brakeman
   end
 end
--- app/models/warning.rb
+++ app/models/warning.rb
@@ -2,4 +2,16 @@ class Warning < ActiveRecord::Base
   attr_accessible :author, :email, :line, :message, :modified_at, :path, :service, :sha1
 
   belongs_to :report
+
+  scope :brakeman, lambda {
+    where(:service => "brakeman")
+  }
+  scope :rails_best_practices, lambda {
+    where(:service => "rails_best_practices")
+  }
+
+  def save(*)
+    self[:modified_at] = Time.at(modified_at + 9.hours) if modified_at.is_a? Fixnum
+    super
+  end
 end
