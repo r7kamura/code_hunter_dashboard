28bc920c52688bee6bddcf880286c75d2ff8532c
Ryo Nakamura <ryo-nakamura@cookpad.com>
2013-01-11 15:25:12 +0900

    Initial commit
    $ rails new code_hunter_dashboard -T -d mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeHunterDashboard::Application.config.secret_token = 'e9c193fcc780db6e5112f581312eacc9b7aca5785f46f1b0f4b86b531b00dc70202dd4c142ad6103bd2fb8ce413fd3d863280aec2ddb9d5dc1e80a042579450b'
