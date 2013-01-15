f325a929856906b6983676ba6f082cd94aa9f847
Ryo Nakamura <ryo-nakamura@cookpad.com>
2013-01-15 12:06:45 +0900

    Add author ranking

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -5,4 +5,13 @@ module ReportsHelper
     rows << reports.map {|report| "#{report.created_at},#{report.send(counter_column_name)}" }
     rows.join("\n")
   end
+
+  def rank_by_author_count(warnings)
+    ranks = {}
+    ranks.default = 0
+    warnings.each_with_object(ranks) do |warning, hash|
+      hash[warning.author] += 1
+    end
+    ranks.sort_by {|author, count| -count }
+  end
 end
