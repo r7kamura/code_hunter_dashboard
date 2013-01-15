1126be3b204a7ed25aaa0c90d7d0a9320c177908
Ryo Nakamura <ryo-nakamura@cookpad.com>
2013-01-15 14:37:41 +0900

    Show commit date count graph

--- app/assets/javascripts/reports.js.coffee
+++ app/assets/javascripts/reports.js.coffee
@@ -1,8 +1,9 @@
 $ ->
-  return unless $('body.reports_controller.dashboard_action').length
+  return unless $('body.reports_controller').length
   $('.chart').each ->
     chart = $(this)
     graph = chart.find('.graph')
     json  = chart.find('.csv').data('json')
+    roll  = graph.data('roll')
     csv   = JSON.parse(json)
-    new Dygraph(graph[0], csv)
+    new Dygraph(graph[0], csv, rollPeriod: roll)
--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,17 +1,25 @@
 module ReportsHelper
-  def to_csv(reports, counter_column_name)
+  def to_count_csv(reports, counter_column_name)
     rows = []
-    rows << ["Time,Count"]
-    rows << reports.map {|report| "#{report.created_at},#{report.send(counter_column_name)}" }
+    rows << "Time,Count"
+    rows += reports.map {|report| "#{report.created_at},#{report.send(counter_column_name)}" }
+    rows.join("\n")
+  end
+
+  def to_commit_week_count_csv(warnings)
+    hash = {}
+    hash.default = 0
+    warnings.each {|warning| hash[warning.modified_at.to_date.beginning_of_week] += 1 }
+    rows = []
+    rows << "Time,Count"
+    rows += hash.map {|date, count| "#{date},#{count}" }
     rows.join("\n")
   end
 
   def rank_by_author_count(warnings)
     ranks = {}
     ranks.default = 0
-    warnings.each_with_object(ranks) do |warning, hash|
-      hash[warning.author] += 1
-    end
+    warnings.each {|warning| ranks[warning.author] += 1 }
     ranks.sort_by {|author, count| -count }
   end
 end
