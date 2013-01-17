cd6a5141c190a05cbaeead32b6ae555dc617bd21
Ryo Nakamura <ryo-nakamura@cookpad.com>
2013-01-17 10:12:15 +0900

    Change github host to GITHUB_HOST

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, Settings.github.client_id, Settings.github.client_secret
+  provider :github, Settings.github.client_id, Settings.github.client_secret,
+    :client_options => {
+      :site          => "https://#{Settings.github.host}/api/v3",
+      :authorize_url => "https://#{Settings.github.host}/login/oauth/authorize",
+      :token_url     => "https://#{Settings.github.host}/login/oauth/access_token",
+    }
 end
