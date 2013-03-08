6eaa19f22161564adee5263006056414092f51ec
Ryo Nakamura <ryo-nakamura@cookpad.com>
2013-03-08 19:32:17 +0900

    Load .env explicitly to take care of loading order

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
+Dotenv.load
+
 module CodeHunterDashboard
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
